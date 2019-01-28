e02e9f9ec79fb08acaa35e84da5cbb9585cc8a4a
George  Pianka <gpianka1@gmail.com>
2019-01-28 04:23:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,8 +11,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find_by_id(params[:id])
-    @student.toggle!(:active)
-    @student.save
+    @blog.toggle(:active).save
     redirect_to student_path(@student)
   end
 
