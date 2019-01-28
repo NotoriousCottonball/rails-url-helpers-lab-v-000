ef7ab930a894621afa808e2a3170de4673d8bda0
George  Pianka <gpianka1@gmail.com>
2019-01-28 04:01:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,26 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
-  def index
-    @students = Student.all
+  before_action :set_student, only: :show	  before_action :set_student, only: :show
+  	
+   def index	  def index
+    @students = Student.all	    @students = Student.all
+  end	  end
+
+
+   def show	  def show
+    @student = Student.find_by_id(params[:id])
   end
 
-  def show
-  end
+   def activate
+    @student = Student.find_by_id(params[:id])
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
+  end	  end
 
-  private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
-end
+   private	  private
+
+
+     def set_student	    def set_student
+      @student = Student.find(params[:id])	      @student = Student.find(params[:id])
+    end	    end
+end 	end
