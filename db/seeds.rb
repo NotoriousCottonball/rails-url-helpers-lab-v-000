102c5a3c5253c3678ead94bef58f0060b65d906b
George  Pianka <gpianka1@gmail.com>
2019-01-28 03:58:16 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-Student.create!(first_name: "Lindsey", last_name: "Stirling"
